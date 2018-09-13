fe31a76a8d7a6599d4eacf945f830a1ccaca682d
Austin Gray <austingray@Austins-MacBook-Pro.local>
2018-09-13 12:09:07 -0700

    adds functional mortgage calculator

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  // your Javascript goes here
+  constructor() {
+    super();
+    this.state = {
+      balance: 0,
+      rate: 0,
+      term: 15,
+      output: 0
+    }
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  calculate({balance, rate, term}) {
+    rate = rate/1200
+    this.setState({output: balance * ((rate*(1+rate)**(term*12))/((1+rate)**(term*12)-1))})
+  }
+
   render() {
     return (
-      <div className='container'>
-        {/* your JSX goes here */}
+      <div className='container form-horizontal'>
+        <h3>Mortgage Calculator</h3>
+        <input name="balance" type="number" value={this.state.balance}
+          onChange={this.handleChange.bind(this)}/>
+        <input name="rate" type="number" step="0.01" value={this.state.rate}
+          onChange={this.handleChange.bind(this)}/>
+        <select name="term" onChange={this.handleChange.bind(this)}>
+          <option value="15">15</option>
+          <option value="30">30</option>
+        </select>
+        <button name="submit" onClick={() => this.calculate.bind(this)(this.state)}>Submit</button>
+        <div name="output" id="output">
+          <p>${this.state.output.toFixed(2)} is your payment.</p>
+        </div>
       </div>
     );
   }
